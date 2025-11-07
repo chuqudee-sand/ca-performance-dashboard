8d7a6e74271b8a06b631c1f3652bbb962324a059
chuqudee-sand <cokereafor@sandtech.com>
2025-11-07 17:45:56 +0100

    Create ChartWrapper.tsx

---
+++ src/components/ChartWrapper.tsx
@@ -0,0 +1,7 @@
+// src/components/ChartWrapper.tsx
+"use client";
+import { ReactNode } from "react";
+
+export default function ChartWrapper({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
