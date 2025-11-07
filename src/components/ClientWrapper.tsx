9d62440280f3e71f5a462729c92bb2d4a54098ba
chuqudee-sand <cokereafor@sandtech.com>
2025-11-07 11:06:47 +0100

    Delete src/components/ClientWrapper.tsx

--- src/components/ClientWrapper.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/ClientWrapper.tsx
-"use client";
-
-import AppProvider from "./AppProvider";
-import { ReactNode } from "react";
-
-export default function ClientWrapper({ children }: { children: ReactNode }) {
-  return <AppProvider>{children}</AppProvider>;
-}
