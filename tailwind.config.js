2f733920b7b8064826a14313d30d44ad413c740d
chuqudee-sand <cokereafor@sandtech.com>
2025-11-07 10:33:21 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
   theme: {
     extend: {
       colors: {
+        darkBg: "#0f172a",
+        darkCard: "#1e293b",
+        darkText: "#e2e8f0",
         alxRed: "#E22D2D",
-        darkBg: "#0D0D0D",
-        darkCard: "#1A1A1A",
-        darkText: "#E2E2E2",
       },
     },
   },
