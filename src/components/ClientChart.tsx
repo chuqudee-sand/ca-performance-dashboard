daac43128531b01bfcc4fa234d97764d9f34b57e
chuqudee-sand <cokereafor@sandtech.com>
2025-11-08 14:43:28 +0100

    Delete src/components/ClientChart.tsx

--- src/components/ClientChart.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/ClientChart.tsx
-"use client";
-
-import { ReactNode } from "react";
-
-export default function ClientChart({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
