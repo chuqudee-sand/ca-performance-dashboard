3ee98644574030f822d528f30583ca7fee7d11c1
chuqudee-sand <cokereafor@sandtech.com>
2025-11-08 12:29:37 +0100

    Create ClientChart.tsx

---
+++ src/components/ClientChart.tsx
@@ -0,0 +1,8 @@
+// src/components/ClientChart.tsx
+"use client";
+
+import { ReactNode } from "react";
+
+export default function ClientChart({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
